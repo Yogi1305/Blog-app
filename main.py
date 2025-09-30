6d1816715ea94ff8f4e60d0b8880b0e4015c1bd8
Yogi1305 <yogesh.22273@knit.ac.in>
2025-09-30 09:09:59 +0530

    cors

--- main.py
+++ main.py
@@ -4,11 +4,32 @@ from models.user import User
 from models.blog import Post
 from controller.user_controller import user_router
 from controller.post_controller import post_router
+from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 
 app=FastAPI()
 
+
+
+
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
 @app.get("/test")
 async def hello():
     return {"message": "Hello World"}
